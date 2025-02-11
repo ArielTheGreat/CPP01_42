3f8efa3eee865eb5e8ed1a8ebddef5e09e26bec4
frocha <frocha@student.42heilbronn.de>
2025-02-11 14:45:19 +0100

    added main for the zombies

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,7 +1,21 @@
 #include "Zombie.hpp"
 
-int main()
+int main(void)
 {
-    
+    int		number_zombies;
+	Zombie	*horde;
+
+	number_zombies = 9;
+	horde = zombieHorde(number_zombies, "francisco");
+	if (horde == nullptr)
+	{
+		std::cerr << "[ERRRO]: Failed to create zombie horde!";
+		return (1);
+	}
+	for (int i = 0; i < number_zombies; i++)
+	{
+		horde[i].announce();
+	}
+	delete[] horde;
     return 0;
 }
