5809e700c8c464e882738e71da251916c638f950
Franco Rocha <franco.rocha@fast2work.de>
2025-02-12 13:29:08 +0100

    added stack object

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,8 +2,11 @@
 
 int main()
 {
+    //Object on Heap
     Zombie *randomZombie = newZombie("Mateo");
     randomZombie->announce();
     delete(randomZombie);
+    //Object on Stack
+	randomChump("Lucia");
     return 0;
 }
