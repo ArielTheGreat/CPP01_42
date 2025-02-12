7a175c956eef358ed1e87fd319b3d3c556cac696
Franco Rocha <franco.rocha@fast2work.de>
2025-02-12 14:12:21 +0100

    added check if number is negative

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -3,9 +3,15 @@
 
 Zombie* zombieHorde( int N, std::string name )
 {
-    Zombie* list_zombies = new Zombie[N];
+    Zombie* list_zombies;
     int counter;
 
+    if (N < 0)
+	{
+		std::cerr << "[Error]: N cannot be negative!" << std::endl;
+		return nullptr;
+	}
+    list_zombies = new Zombie[N];
     counter = 0;
     while(counter < N)
     {
