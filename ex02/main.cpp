28b051bf65fba8a6120435bac32a69e37f3ed6fc
Franco Rocha <franco.rocha@fast2work.de>
2025-02-13 01:18:57 +0100

    improved readability

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 int main()
 {
     std::string str = "HI THIS IS BRAIN";
     std::string* stringPTR = &str;
     std::string& stringREF = str;
 
-    std::cout << &str << std::endl;
-    std::cout << stringPTR << std::endl;
-    std::cout << &stringREF << std::endl;
+    std::cout << "Memory address of str: " << &str << std::endl;
+    std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
 
-    std::cout << str << std::endl;
-    std::cout << *stringPTR << std::endl;
-    std::cout << stringREF << std::endl;
+    std::cout << "Value of str: " << str << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
     return 0;
 }
